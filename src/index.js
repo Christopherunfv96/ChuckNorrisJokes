45c9c5e47bb06e228728c0d30bd2a15ef5ccff38
Christopherunfv96 <57688565+Christopherunfv96@users.noreply.github.com>
2020-04-12 13:37:53 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,2 @@
-//const jokeUrl = "https://api.chucknorris.io/jokes/random";
-
-//import { obtenerChiste } from "./js/http-provider";
 import { init } from "./js/chistes-page";
-
-// import * as CRUD from './js/crud-provider';
-
 init();
-
-// fetch(jokeUrl).then((response) => {
-// 	//respuesta
-// 	response.json().then((data) => {
-// 		console.log(data.id);
-// 	});
-// });
-//* otra manera mas corta y eficiente
-// fetch(jokeUrl)
-// 	.then((response) => response.json())
-// 	.then((data) => console.log(data));
-
-// obtenerChiste().then((data) => console.log(data));
-// obtenerChiste().then(console.log);
